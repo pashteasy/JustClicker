e187bd52778739bb368d1154356c24f8d816b8a0
pashtzt <80247775+pashtzt@users.noreply.github.com>
2022-12-02 00:12:24 +0300

    Update and rename autoclicker with UI.py to JustClicker.py

--- JustClicker.py
+++ JustClicker.py
@@ -1,17 +1,17 @@
 import pyautogui as auto
 import time
 import keyboard as key
+import random
 from tkinter import *
 from tkinter import messagebox
 
-#окно
 window = Tk()
-window.title("Autoclicker by pashtzt")
+window.title("JustClicker")
 window.geometry('320x140')
-#текст 
-lb1 = Label(window, text = 'Интервал нажатия (c):', font=("Arial Bold", 12),)
-lb2 = Label(window, text = 'Старт и пауза:', font=("Arial Bold", 12))
-lb3 = Label(window, text = 'Выйти из режима кликера:', font=("Arial Bold", 12))
+
+lb1 = Label(window, text = 'Click interval (s):', font=("Arial Bold", 12),)
+lb2 = Label(window, text = 'Hot key start/pause:', font=("Arial Bold", 12))
+lb3 = Label(window, text = 'Exit clicker mode:', font=("Arial Bold", 12))
 lb4 = Label(window, text = 'Space', font=("Arial Bold", 12))
 lb5 = Label(window, text = 'Esc', font=("Arial Bold", 12))
 lb1.grid(column=0, row=0 )
@@ -19,11 +19,16 @@ lb2.grid(column=0, row=1)
 lb3.grid(column=0, row=2)
 lb4.grid(column=1, row=1)
 lb5.grid(column=1, row=2)
-#выбор задержки
+
 txt = Entry(window, width=7)
 txt.grid(column=1, row=0)  
 
-def click(t): 
+chk = Checkbutton(window, text='Random delay')
+chk.grid(column=1, row=3)
+
+def click(t):
+    if chk.getboolean:
+        t = t + random.uniform(0,t)
     time.sleep(t)     
     auto.click()
 
@@ -40,10 +45,10 @@ def main():
             if isClicking:  
                 click(t)
     except ValueError:
-        messagebox.showerror('Ошибка', 'Введите корректное значение типа Float')
+        messagebox.showerror('Error', 'Enter a valid Float value')
     
         
-btn = Button(window, text = 'Запустить', command=main, font=("Arial Bold", 12))
+btn = Button(window, text = 'Start', command=main, font=("Arial Bold", 12))
 btn.grid(column=0, row=3) 
 txt.focus()
 isClicking = False
